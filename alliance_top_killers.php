4293a5099a2fae699e663cb3e0537a22c1814a1c
botis_t <boris@talovikov.ru>
2014-08-02 23:20:02 +0600

    code rewiew

--- alliance_top_killers.php
+++ alliance_top_killers.php
@@ -53,99 +53,116 @@ class TopList_AllianceLosses extends TopList_Base {
 
 }
 
-global $sourcePage;
+class alliance_top_killers {
 
-$pilot_id = config::get('cfg_pilotid');
-$corp_id = config::get('cfg_corpid');
-$alliance_id = config::get('cfg_allianceid');
-$limit = config::get('alliance_top_killers_limit');
-
-if (empty($limit)) {
-  config::set('alliance_top_killers_limit', 10);
-  $limit = 10;
-}
-
-if ($sourcePage->getView() == 'losses') {
-  $all_top = new TopList_AllianceLosses();
-  if(!empty($alliance_id)) {
-    $all_top->addVictimAlliance($alliance_id);
+  function add($page) {
+    $page->addBefore('topLists', 'alliance_top_killers::show');
   }
-  if(!empty($corp_id)) {
-    $all_top->addVictimCorp($corp_id);
-  }
-  if(!empty($pilot_id)) {
-    $all_top->addVictimPilot($pilot_id);
-  }
-  $award_img = '/awards/moon.png';
-  $smarty->assign('title', "Top Alliance Losers");
-}
-else {
-  $all_top = new TopList_AllianceKills();
-  if(!empty($alliance_id)) {
-    $all_top->addInvolvedAlliance($alliance_id);
-  }
-  if(!empty($corp_id)) {
-    $all_top->addInvolvedCorp($corp_id);
-  }
-  if(!empty($pilot_id)) {
-    $all_top->addInvolvedPilot($pilot_id);
-  }
-  $award_img = '/awards/eagle.png';
-  $smarty->assign('title', "Top Alliance Killers");
-}
 
-$all_top->setPodsNoobShips(config::get('podnoobs'));
-$all_top->setLimit($limit);
-$all_top->generate();
-$sourcePage->loadTime($all_top);
+  function show($page) {
 
-$row = $all_top->getRow();
-$max_kl = $row['cnt'];
-$alliance = new Alliance($row['all_id']);
-$bar = new BarGraph($row['cnt'], $max_kl);
+    global $smarty;
 
-$smarty->assign('corp_portrait', $alliance->getPortraitURL(64));
-$smarty->assign('award_img', config::get('cfg_img') . $award_img);
-$smarty->assign('url', $alliance->getDetailsURL());
-$smarty->assign('name', $alliance->getName());
-$smarty->assign('bar', $bar->generate());
-$smarty->assign('cnt', $row['cnt']);
+    $pilot_id = config::get('cfg_pilotid');
+    $corp_id = config::get('cfg_corpid');
+    $alliance_id = config::get('cfg_allianceid');
+    $limit = config::get('alliance_top_killers_limit');
 
-$i = 1;
+    if (empty($limit)) {
+      config::set('alliance_top_killers_limit', 10);
+      $limit = 10;
+    }
 
-while(($row = $all_top->getRow()) && ($i < $limit)) {
-  $alliance = new Alliance($row['all_id']);
-  $bar = new BarGraph($row['cnt'], $max_kl);
-  $top[$i + 1] = array(
-    'url' => $alliance->getDetailsURL(),
-    'name' => $alliance->getName(),
-    'bar' => $bar->generate(),
-    'cnt' => $row['cnt'],
-  );
-  $i++;
-}
+    if ($page->getView() == 'losses') {
+      $all_top = new TopList_AllianceLosses();
+      if(!empty($alliance_id)) {
+        $all_top->addVictimAlliance($alliance_id);
+      }
+      if(!empty($corp_id)) {
+        $all_top->addVictimCorp($corp_id);
+      }
+      if(!empty($pilot_id)) {
+        $all_top->addVictimPilot($pilot_id);
+      }
+      $award_img = '/awards/moon.png';
+      $smarty->assign('title', "Top Alliance Losers");
+    }
+    else {
+      $all_top = new TopList_AllianceKills();
+      if(!empty($alliance_id)) {
+        $all_top->addInvolvedAlliance($alliance_id);
+      }
+      if(!empty($corp_id)) {
+        $all_top->addInvolvedCorp($corp_id);
+      }
+      if(!empty($pilot_id)) {
+        $all_top->addInvolvedPilot($pilot_id);
+      }
+      $award_img = '/awards/eagle.png';
+      $smarty->assign('title', "Top Alliance Killers");
+    }
 
-$smarty->assign('top', $top);
-$smarty->assign('comment', "kills in " . date('F, Y', mktime(0, 0, 0, getMonth_atk(), 1, getYear_atk())));
+    $all_top->setPodsNoobShips(config::get('podnoobs'));
+    $all_top->setLimit($limit);
+    $all_top->generate();
+    $page->loadTime($all_top);
 
-function getMonth_atk() {
-  $month = edkURI::getArg('m');
-  $week = edkURI::getArg('w');
-  if (!empty($month)) {
-    return $month;
+    $row = $all_top->getRow();
+
+    if(isset($row)) {
+
+      $max_kl = $row['cnt'];
+      $alliance = new Alliance($row['all_id']);
+      $bar = new BarGraph($row['cnt'], $max_kl);
+
+      $smarty->assign('corp_portrait', $alliance->getPortraitURL(64));
+      $smarty->assign('award_img', config::get('cfg_img') . $award_img);
+      $smarty->assign('url', $alliance->getDetailsURL());
+      $smarty->assign('name', $alliance->getName());
+      $smarty->assign('bar', $bar->generate());
+      $smarty->assign('cnt', $row['cnt']);
+
+      $i = 1;
+
+      while(($row = $all_top->getRow()) && ($i < $limit)) {
+        $alliance = new Alliance($row['all_id']);
+        $bar = new BarGraph($row['cnt'], $max_kl);
+        $top[$i + 1] = array(
+          'url' => $alliance->getDetailsURL(),
+          'name' => $alliance->getName(),
+          'bar' => $bar->generate(),
+          'cnt' => $row['cnt'],
+        );
+        $i++;
+      }
+
+      $smarty->assign('top', $top);
+      $smarty->assign('comment', "kills in " . date('F, Y', mktime(0, 0, 0, self::getMonth_atk(), 1, self::getYear_atk())));
+      return $smarty->fetch(get_tpl('./mods/alliance_top_killers/alliance_top_killers'));
+    }
+
   }
-  elseif(!empty($week)) {
-    return date('m', mktime(0, 0, 0, 1, (($week - 1) * 7) + 1, getYear_atk()));
-  }
-  else {
-    return kbdate('m');
-  }
-}
 
-function getYear_atk() {
-  $year = edkURI::getArg('y');
-  if(empty($year)) {
-    $year = kbdate('Y');
+  private function getMonth_atk() {
+    $month = edkURI::getArg('m');
+    $week = edkURI::getArg('w');
+    if (!empty($month)) {
+      return $month;
+    }
+    elseif(!empty($week)) {
+      return date('m', mktime(0, 0, 0, 1, (($week - 1) * 7) + 1, self::getYear_atk()));
+    }
+    else {
+      return kbdate('m');
+    }
   }
-  return $year;
+
+  private function getYear_atk() {
+    $year = edkURI::getArg('y');
+    if(empty($year)) {
+      $year = kbdate('Y');
+    }
+    return $year;
+  }
+
 }
--- init.php
+++ init.php
@@ -7,27 +7,11 @@
  * @version 1.1p2
  */
 
+require_once('mods/alliance_top_killers/alliance_top_killers.php');
+
 event::register('home_context_assembling', 'alliance_top_killers::add');
 
 $modInfo['alliance_top_killers']['name'] = 'Alliance Top Killers';
 $modInfo['alliance_top_killers']['abstract'] = '';
 $modInfo['alliance_top_killers']['about'] = 'It\'s fork <a href="http://www.evekb.org/forum/viewtopic.php?f=505&t=18523">Corp Top Killers Mod</a>. Fork by <a href="https://github.com/6RUN0">boris_t</a>.<br />
 <a href="https://github.com/6RUN0/alliance_top_killers">Get Alliance Top Killers</a>';
-
-
-
-class alliance_top_killers {
-
-  function add($page) {
-    global $sourcePage;
-    $sourcePage = $page;
-    $page->addBefore('topLists', 'alliance_top_killers::show');
-  }
-
-  function show() {
-    global $smarty;
-    include_once('mods/alliance_top_killers/alliance_top_killers.php');
-    $html .= $smarty->fetch(get_tpl('./mods/alliance_top_killers/alliance_top_killers'));
-    return $html;
-  }
-}
